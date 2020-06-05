0780f2c62006d8af6f5cadc988e55a03bc735c74
Claudio Bonesana <claudio.bonesana@supsi.ch>
2020-06-05 21:14:14 +0200

    CSS for unit management.

--- code/web/server/static/js/game.js
+++ code/web/server/static/js/game.js
@@ -2,26 +2,36 @@ let figures = undefined;
 
 function addFigure(data, agent) {
     let active = data.activated ? 'activated' : 'notActivated';
-    let responded = data.activated ? 'responded' : 'notresponded';
+    let responded = data.activated ? 'responded' : 'notResponded';
     let killed = data.killed ? 'killed' : '';
 
-    let weapons = $('<p/>').addClass('uWeapons')
+    let uData = $('<div/>').addClass('uData')
+        .append($('<div/>').addClass('uName').text(`${data.name}`))
+        .append($('<div/>').addClass('uKind').text(`(${data.kind})`))
+        .append($('<div/>').addClass('uPos').text(`(${data.i}, ${data.j})`))
+        .append($('<div/>').addClass('uHP').text(`HP: ${data.hp}`))
+        .append($('<div/>').addClass('uLoad').text(`LOAD: ${data.load}`))
+        .append($('<div/>').addClass('uMove').text(`MOVE: ${data.move}`))
+        .append($('<div/>').addClass('uStat').text(`STAT: ${data.stat}`))
+
+    let uWeapons = $('<div/>').addClass('uWeapons')
     data.weapons.forEach(function (item, _) {
         let effect = item.no_effect ? 'wNoEffect' : '';
         let ammo = item.ammo > 1000000 ? '♾' : item.ammo;
+        let range = item.max_range > 1000000 ? '♾' : item.range;
         let curved = item.curved ? 'C' : ''; // TODO: find an image
         let antiTank = item.antitank ? 'T' : ''; // TODO: find an image
 
-        weapons.append(
+        uWeapons.append(
             $('<div/>')
-                .attr('id', 'w' + item.id)
+                .addClass('w' + item.id)
                 .addClass(effect)
                 .addClass('weapon')
                 .append($('<div/>').addClass('wName').text(item.name))
                 .append($('<div/>').addClass('wAmmo').text(ammo))
                 .append($('<div/>').addClass('wAtk').text(item.atk_normal))
                 .append($('<div/>').addClass('wResp').text(item.atk_response))
-                .append($('<div/>').addClass('wRange').text(item.max_range))
+                .append($('<div/>').addClass('wRange').text(range))
                 .append($('<div/>').addClass('wCurved').text(curved))
                 .append($('<div/>').addClass('wAntitank').text(antiTank))
         )
@@ -35,14 +45,8 @@ function addFigure(data, agent) {
             .addClass(responded)
             .addClass(data.kind)
             .addClass('unit')
-            .append($('<div/>').addClass('uName').text(`${data.name}`))
-            .append($('<div/>').addClass('uKind').text(`(${data.kind})`))
-            .append($('<div/>').addClass('uPos').text(`(${data.i}, ${data.j})`))
-            .append($('<div/>').addClass('uHP').text(`HP: ${data.hp}`))
-            .append($('<div/>').addClass('uLoad').text(`LOAD: ${data.load}`))
-            .append($('<div/>').addClass('uMove').text(`MOVE: ${data.move}`))
-            .append($('<div/>').addClass('uStat').text(`STAT: ${data.stat}`))
-            .append(weapons)
+            .append(uData)
+            .append(uWeapons)
     );
 }
 
