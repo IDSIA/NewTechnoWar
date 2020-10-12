b7bc0013e432b7781b5fcdd6d798490a047c012e
Claudio Bonesana <claudio.bonesana@supsi.ch>
2020-10-12 09:57:31 +0200

    First collect then parse in config_reader.py. Added dummy scenarios and battlegrounds. Added basic figure status.

--- code/config_reader.py
+++ code/config_reader.py
@@ -5,7 +5,7 @@ from collections.abc import Mapping
 
 from core.const import BLUE, RED
 from core.figures import Figure, Weapon
-from core.figures.status import HIDDEN, LOADED, NO_EFFECT, IN_MOTION, UPSTAIRS, UNDER_FIRE, CUT_OFF
+from core.figures.status import FigureStatus
 from core.game.board import GameBoard
 from core.game.goals import GoalEliminateOpponent, GoalDefendPoint, GoalMaxTurn, GoalReachPoint
 from core.game.state import GameState
@@ -14,35 +14,48 @@ from scenarios.utils import fillLine
 from utils import INFINITE
 from utils.copy import deepcopy
 
-WEAPONS_TMPL = {}
-FIGURES_TMPL = {}
+TMPL_WEAPONS = {}
+TMPL_FIGURES = {}
+TMPL_FIGURES_STATUS_TYPE = {}
+TMPL_TERRAIN_TYPE = {}
+TMPL_BOARDS = {}
+TMPL_SCENARIOS = {}
+
 FIGURES_TYPE = {'other': 0, 'infantry': 1, 'vehicle': 2}
-FIGURES_STATUS_TYPE = {
-    'HIDDEN': HIDDEN,
-    'LOADED': LOADED,
-    'NO_EFFECT': NO_EFFECT,
-    'IN_MOTION': IN_MOTION,
-    'UPSTAIRS': UPSTAIRS,
-    'UNDER_FIRE': UNDER_FIRE,
-    'CUT_OFF': CUT_OFF,
-}
+FIGURES_STATUS_TYPE = {}
+
 TERRAIN_TYPE = {}
 BOARDS = {}
 SCENARIOS = {}
 
 
-def parse_figure(data: dict):
+def collect_figure_status(data: dict):
+    for name, fData in data['status'].items():
+        TMPL_FIGURES_STATUS_TYPE[name] = fData
+
+
+def parse_figure_status():
+    for name, fData in TMPL_FIGURES_STATUS_TYPE.items():
+        FIGURES_STATUS_TYPE[name] = FigureStatus(fData['name'], fData['value'])
+
+
+def collect_figure(data: dict):
     for name, fData in data['figure'].items():
-        FIGURES_TMPL[name] = fData
+        TMPL_FIGURES[name] = fData
 
 
-def parse_weapons(data: dict):
+def collect_weapons(data: dict):
     for name, wData in data['weapon'].items():
-        WEAPONS_TMPL[name] = wData
+        TMPL_WEAPONS[name] = wData
 
 
-def parse_terrain(data: dict):
+def collect_terrain(data: dict):
     for name, tData in data['terrain'].items():
+        TMPL_TERRAIN_TYPE[name] = tData
+
+
+def parse_terrain():
+    for name, tData in TMPL_TERRAIN_TYPE.items():
         TERRAIN_TYPE[name] = Terrain(
             len(TERRAIN_TYPE),
             tData['name'],
@@ -53,8 +66,16 @@ def parse_terrain(data: dict):
         )
 
 
-def parse_board(data: dict):
+def collect_board(data: dict):
     for name, bData in data['board'].items():
+        TMPL_BOARDS[name] = bData
+
+
+def parse_board():
+    for name, bData in TMPL_BOARDS.items():
+        if 'template' in bData:
+            bData = update(deepcopy(TMPL_BOARDS[bData['template']]), bData)
+
         shape = tuple(bData['shape'])
 
         board = GameBoard(shape, name)
@@ -70,6 +91,29 @@ def parse_board(data: dict):
                     fillLine(terrain, elem['line'][0], elem['line'][1], t.level)
                 if 'point' in elem:
                     terrain[elem['point']] = t.level
+                if 'column' in elem:
+                    c = elem['column']
+                    if isinstance(c, Mapping):
+                        if 'start' in c and 'end' not in c:
+                            terrain[c['start']:, :] = t.level
+                        elif 'start' not in c and 'end' in c:
+                            terrain[:c['end'], :] = t.level
+                        elif 'start' in c and 'end' in c:
+                            terrain[c['start']:c['end'], :] = t.level
+                    else:
+                        terrain[c, :] = t.level
+
+                if 'row' in elem:
+                    r = elem['row']
+                    if isinstance(r, Mapping):
+                        if 'start' in r and 'end' not in r:
+                            terrain[:, r['start']:] = t.level
+                        elif 'start' not in r and 'end' in r:
+                            terrain[:, r['end']] = t.level
+                        elif 'start' in r and 'end' in r:
+                            terrain[:, r['start']:r['end']] = t.level
+                    else:
+                        terrain[:, r] = t.level
 
 
 def update(d: dict, u: Mapping):
@@ -81,8 +125,13 @@ def update(d: dict, u: Mapping):
     return d
 
 
-def parse_scenario(data: dict):
+def collect_scenario(data: dict):
     for name, sData in data['scenario'].items():
+        TMPL_SCENARIOS[name] = sData
+
+
+def parse_scenario():
+    for name, sData in TMPL_SCENARIOS.items():
         board: GameBoard = BOARDS[sData['map']]
         state: GameState = GameState(board.shape, name)
 
@@ -111,8 +160,8 @@ def parse_scenario(data: dict):
             for f in sData[team]['figures']:
                 # setup figures
                 for name, fData in f.items():
-                    s = FIGURES_STATUS_TYPE[fData['status']] if 'status' in fData else NO_EFFECT
-                    t = update(deepcopy(FIGURES_TMPL['template']), FIGURES_TMPL[fData['type']])
+                    s = FIGURES_STATUS_TYPE[fData['status']] if 'status' in fData else FIGURES_STATUS_TYPE['NO_EFFECT']
+                    t = update(deepcopy(TMPL_FIGURES['template']), TMPL_FIGURES[fData['type']])
                     t['kind'] = FIGURES_TYPE[t.pop('type', None)]
                     t['hp_max'] = t['hp']
 
@@ -121,7 +170,7 @@ def parse_scenario(data: dict):
                         if k == 'weapons':
                             # setup weapons
                             for wName, wData in v.items():
-                                tw = update(deepcopy(WEAPONS_TMPL['template']), WEAPONS_TMPL[wName])
+                                tw = update(deepcopy(TMPL_WEAPONS['template']), TMPL_WEAPONS[wName])
                                 tw['ammo'] = INFINITE if wData == 'inf' else wData
                                 tw['ammo_max'] = tw['ammo']
 
@@ -142,8 +191,9 @@ def parse_scenario(data: dict):
 
 if __name__ == '__main__':
 
-    dirs = ['terrains', 'maps', 'weapons', 'figures', 'scenarios']
+    dirs = ['terrains', 'maps', 'weapons', 'status', 'figures', 'scenarios']
 
+    # TODO: first collect, then parse
     for dir in dirs:
         path = os.path.join('config', dir)
         for name in os.listdir(path):
@@ -151,19 +201,27 @@ if __name__ == '__main__':
                 data = yaml.safe_load(f)
 
                 if 'terrain' in data:
-                    parse_terrain(data)
+                    collect_terrain(data)
 
                 if 'board' in data:
-                    parse_board(data)
+                    collect_board(data)
 
                 if 'weapon' in data:
-                    parse_weapons(data)
+                    collect_weapons(data)
+
+                if 'status' in data:
+                    collect_figure_status(data)
 
                 if 'figure' in data:
-                    parse_figure(data)
+                    collect_figure(data)
 
                 if 'scenario' in data:
-                    parse_scenario(data)
+                    collect_scenario(data)
+
+    parse_figure_status()
+    parse_terrain()
+    parse_board()
+    parse_scenario()
 
     for name, scenario in SCENARIOS.items():
         board, state = scenario
