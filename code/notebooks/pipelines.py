db49d212b6561189b6537cbd50c3a5a0b8967570
Nicol Allegra <nicol.allegra@student.supsi.ch>
2020-11-09 18:54:04 +0100

    modifica creazione modelli

--- code/notebooks/pipelines.py
+++ code/notebooks/pipelines.py
@@ -2,13 +2,14 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC, NuSVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
 import pandas as pd
 from sklearn.naive_bayes import GaussianNB
+#import os.path as op
 
 import joblib
+#dir_path = op.dirname(op.realpath(__file__))
 
 
 def pipelineClassifier(df, name):
@@ -32,17 +33,28 @@ def pipelineClassifier(df, name):
         pipe = Pipeline(steps=[('preprocessor', preprocessor),
                                ('classifier', classifier)])
         pipe.fit(X, y)
-        file_name = f'models/{name}_{classifier.__class__.__name__}.joblib'
+        file_name = f'{name}_{classifier.__class__.__name__}.joblib'
         joblib.dump(pipe, file_name)
 
 
 if __name__ == '__main__':
-    dataframes = {"BridgeHead": "../../../data.scenarioBridgeHead.pkl.gz",
+    '''dataframes = {"BridgeHead": "../../../data.scenarioBridgeHead.pkl.gz",
                   "CrossingTheCity": "../../../data.scenarioCrossingTheCity.pkl.gz",
                   "Junction": "../../../data.scenarioJunction.pkl.gz",
                   "JunctionExo": "../../../data.scenarioJunctionExo.pkl.gz",
                   "Roadblock": "../../../data.scenarioRoadblock.pkl.gz",
-                  "Test1v1": "../../../data.scenarioTest1v1.pkl.gz", "Test2v2": "../../../data.scenarioTest2v2.pkl.gz"}
-    pilots = ["BridgeHead", "CrossingTheCity", "Junction", "JunctionExo", "Roadblock", "Test1v1", "Test2v2"]
+                  "Test1v1": "../../../data.scenarioTest1v1.pkl.gz", "Test2v2": "../../../data.scenarioTest2v2.pkl.gz"}'''
+    dataframes = {"Junction": "../../../data.2020-11-09.scenarioJunction.pkl.gz",
+                  "JunctionExo": "../../../data.2020-11-09.scenarioJunctionExo.pkl.gz",
+                  "Test1v1": "../../../data.2020-11-09.scenarioTest1v1.pkl.gz",
+                  "Test2v2": "../../../data.2020-11-09.scenarioTest2v2.pkl.gz"}
+    # pilots = ["BridgeHead", "CrossingTheCity", "Junction", "JunctionExo", "Roadblock", "Test1v1", "Test2v2"]
+    pilots = ["Junction", "JunctionExo", "Test1v1", "Test2v2"]
+
     for p in pilots:
-        pipelineClassifier(pd.read_pickle(dataframes[p]), p)
+        df = pd.read_pickle(dataframes[p])
+        '''df = df.loc[(((df.meta_p_red == "GreedyAgent") & (df.meta_p_blue == "GreedyAgent")) | (
+                    (df.meta_p_red == "GreedyAgent") & (df.meta_p_blue == "RandomAgent")) | (
+                                 (df.meta_p_red == "RandomAgent") & (df.meta_p_blue == "GreedyAgent")))]'''
+
+        pipelineClassifier(df, p)
