a5962b49d97e331136904237383e8592e827785e
Claudio Bonesana <claudio.bonesana@supsi.ch>
2021-02-17 10:00:41 +0000

    Fiexed entropy, added turn to agent df

--- code/agents/utils.py
+++ code/agents/utils.py
@@ -1,10 +1,21 @@
-import numpy as np
+from math import log
 
 
 def entropy(values: list) -> float:
-    values = [v for v in values if v >= 0]
-    if len(values) > 1 and sum(values) > 0:
-        norm = [float(i) / sum(values) for i in values]
-        return -(norm * np.log(norm)).sum() / np.log(len(values))
-    else:
+    n = len(values)
+    if n <= 1:
         return 0.0
+    
+    m = min(values)
+    values = [v+m for v in values]
+    
+    s = sum(values)
+    if s == 0:
+        return 0.0
+    
+    # normalize [0,1]
+    values = [v/s for v in values]
+    
+    h = -sum(v * log(v) for v in values if v > 0)
+    
+    return h/log(n)
