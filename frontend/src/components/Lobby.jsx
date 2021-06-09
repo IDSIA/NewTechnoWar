22d2961c29d9a254154388d34ce305bc40b5eb25
Claudio Bonesana <cbonesana@gmail.com>
2021-06-09 16:31:23 +0200

    Removed link to old guide. Added link to repo.

--- frontend/src/components/Lobby.jsx
+++ frontend/src/components/Lobby.jsx
@@ -148,9 +148,9 @@ export default class Lobby extends React.Component {
 
                             <input type="submit" value="Play" onClick={(event) => this.handleSubmit(event)} />
 
-                            <a className="guide" href={`${API}static/ntw-gui.pdf`} target="_blank">Help</a>
+                            <a className="guide" href="/config">Templates</a>
 
-                            <a className="guide" href={`/config`} target="_blank">Templates</a>
+                            <a className="guide" href="https://github.com/IDSIA/NewTechnoWar">Code repository</a>
                         </div>
                     </form>
                 </div>
